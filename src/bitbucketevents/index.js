5121be2b6e1eb2f8e3c6775144b0de9711e51cc2
Johan Horvius <drhorv@gmail.com>
2020-07-11 16:55:25 +0200

    Find PR identifier in approve PRs

--- src/bitbucketevents/index.js
+++ src/bitbucketevents/index.js
@@ -1,3 +1,8 @@
+const getPrIdentifier = (body) => {
+  const prLink = body["pullrequest"]["links"]["self"]["href"];
+  return prLink.split("/").slice(5).join("/");
+};
+
 /**
  * Responds to any HTTP request.
  *
@@ -7,6 +12,17 @@
 exports.handler = (req, res) => {
   console.log("headers: ", JSON.stringify(req.headers));
   console.log("body: ", JSON.stringify(req.body));
+
+  const eventKey = headers["x-event-key"];
+  if (eventKey === "pullrequest:approved") {
+    const pr = getPrIdentifier(body);
+    console.log("PR " + pr + " was approved");
+  }
+
+  if (eventKey === "pullrequest:unapproved") {
+    const pr = getPrIdentifier(body);
+    console.log("PR " + pr + " was unapproved");
+  }
 
   res.status(200).send({});
 };
