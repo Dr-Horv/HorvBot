b2aa512feca47ce8fe4ad4306e969b39a0ce0fda
Johan Horvius <drhorv@gmail.com>
2020-07-11 15:48:16 +0200

    Added bitbucket function

---
+++ src/bitbucketevents/index.js
@@ -0,0 +1,12 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.handler = (req, res) => {
+  console.log("headers: " + req.headers);
+  console.log("body: " + req.body);
+
+  res.status(200).send({});
+};
